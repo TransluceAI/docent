38244cbc262091864d7f83d830c3b0f604cc512a
Kevin Meng <kevin.meng.01@gmail.com>
2025-07-21 22:37:22 -0400

    Add metadata naming (#240)
    * md naming
    
    * Add migration from prod to main

--- docent_core/_db_service/schemas/base.py
+++ docent_core/_db_service/schemas/base.py
@@ -1,7 +1,18 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
+
+convention = {
+    "ix": "ix_%(table_name)s__%(column_0_N_name)s",
+    "uq": "uq_%(table_name)s__%(column_0_N_name)s",
+    "ck": "ck_%(table_name)s__%(constraint_name)s",
+    "fk": "fk_%(table_name)s__%(column_0_N_name)s__%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
 
 
 class SQLABase(DeclarativeBase):
+    metadata = MetaData(naming_convention=convention)
+
     def dict(self):
         return {c.key: getattr(self, c.key) for c in self.__table__.columns}
 
