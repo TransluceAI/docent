a95f5365bec0789062d81bdd6929b473f186eb65
Kevin Meng <kevin.meng.01@gmail.com>
2025-09-23 18:44:35 -0400

    chore: cleanup old comments

--- docent_core/_web/app/dashboard/[collection_id]/components/chat/ChatArea.tsx
+++ docent_core/_web/app/dashboard/[collection_id]/components/chat/ChatArea.tsx
@@ -111,19 +111,6 @@ export function ChatArea({
     return ans;
   }, [messages, showThinkingSpacer, isSendingMessage, streamingMessageIdx]);
 
-  // // Scroll once when the thinking spacer first appears in the message history (i.e., upon send)
-  // const prevShowThinking = useRef(false);
-  // useEffect(() => {
-  //   if (displayedMessages.length === 0) return;
-  //   const curShowThinking =
-  //     displayedMessages[displayedMessages.length - 1].props
-  //       .isLoadingPlaceholder === true;
-  //   if (!prevShowThinking.current && curShowThinking) {
-  //     scrollToBottom('smooth');
-  //   }
-  //   prevShowThinking.current = curShowThinking;
-  // }, [displayedMessages, scrollToBottom]);
-
   // Auto-scroll when the thinking spacer first appears in the message history (i.e., upon send)
   const lastMessageIsThinkingSpacer = useMemo(() => {
     return (
